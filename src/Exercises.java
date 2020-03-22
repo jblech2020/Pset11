35ff650749126c689c9463b059f86a10531e8879
Jaedan Blechinger-Slocum <jblech2020@gmail.com>
2020-03-22 12:20:20 -0400

    15/25

--- src/Exercises.java
+++ src/Exercises.java
@@ -103,7 +103,21 @@ public class Exercises {
 			return null;
 		}
 	  
-	  return null;
+	  for (int i = 0; i < list.size() - 1; i++) {
+			for (int j = 0; j < list.size() - 1 - i; j++) {
+				if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+					String temp = list.get(j);
+					list.set(j, list.get(j + 1));
+					list.set(j + 1, temp);
+				}
+			}
+		}
+
+		if (!ascending) {
+			Collections.reverse(list);
+		}
+
+		return list;
   }
 
   public ArrayList<Integer> insertion(ArrayList<Integer> list, boolean ascending) {
