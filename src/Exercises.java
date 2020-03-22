95a50871ad5cb8ea4948250a2e246d94b2dc16da
Jaedan Blechinger-Slocum <jblech2020@gmail.com>
2020-03-22 12:11:23 -0400

    12/25

--- src/Exercises.java
+++ src/Exercises.java
@@ -75,6 +75,16 @@ public class Exercises {
 			return null;
 		}
 	  
+	  for (int i = 0; i < list.length - 1; i++) {
+			for (int j = 0; j < list.length - i - 1; j++) {
+				if (list[j] > list[j + 1]) {
+					int temp = list[j];
+					list[j] = list[j + 1];
+					list[j + 1] = temp;
+				}
+			}
+	  }
+	  
 	  return null;
   }
 
